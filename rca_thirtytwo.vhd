c9d4bdd3f10951f8ced895c0c262f39e35a62e41
Vasudevan <vasu3797@gmail.com>
2019-10-25 08:20:07 -0500

    Add files via upload

--- rca_thirtytwo.vhd
+++ rca_thirtytwo.vhd
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.ALL;
 
-entity rca_thitytwo is 
+entity rca_thirtytwo is 
 port ( a : in STD_LOGIC_VECTOR (31 downto 0); --initialising an array 
 	b : in STD_LOGIC_VECTOR (31 downto 0); 
 	cin : in STD_LOGIC;
@@ -19,6 +19,11 @@ port (	a : in STD_LOGIC_VECTOR (3 downto 0); --initialising an array
 	cout : out STD_LOGIC);
 end component fourbit_rca;
 
-signal carry_in : STD_LOGIC_VECTOR (31 downto 0);
+signal carry_in : STD_LOGIC_VECTOR (8 downto 0);
+
+begin
+
+rca_1: fourbit_rca port map( a, b, cin, s, carry_in(0));
+rca_2: fourbit_rca port map( a, b, carry_in(0), 
 
 
